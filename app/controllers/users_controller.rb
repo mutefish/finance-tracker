c66a8973756eb5af83fd10bd12c5535587a41a8b
Karen Ho <karenho008@gmail.com>
2020-09-02 23:55:53 -0400

    Search friendssubmission and display response

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,23 @@ class UsersController < ApplicationController
   end
 
   def search
-    render json: params[:friend]
+    if params[:friend].present?
+      @friend = params[:friend]
+      if @friend
+          respond_to do |format|
+              format.js { render partial: 'users/friend_result' }
+          end
+      else 
+          respond_to do |format|
+              flash.now[:alert] = "Cannot find user!"
+              format.js { render partial: 'users/friend_result' }
+          end
+      end
+  else 
+      respond_to do |format|
+          flash.now[:alert] = "Please enter a user to search"
+          format.js { render partial: 'users/friend_result' }
+      end
+  end
   end
 end
