1e355eed900c585e90f25885db9c30fb765a3e82
Karen Ho <karenho008@gmail.com>
2020-09-04 23:20:28 -0400

    Remove current user from friend search results

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,9 @@ class UsersController < ApplicationController
 
   def search
     if params[:friend].present?
-      @friend = User.search(params[:friend])
-      if @friend.any?
+      @friends = User.search(params[:friend])
+      @friends = current_user.except_current_user(@friends)
+      if @friends.any?
           respond_to do |format|
               format.js { render partial: 'users/friend_result' }
           end
