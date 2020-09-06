9ec82c327ca15b136bfdacbbf6c59afb76cc803e
Karen Ho <karenho008@gmail.com>
2020-09-06 01:49:21 -0400

    Show stocks on searched users profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def my_portfolio
       @tracked_stocks = current_user.stocks
+      @user = current_user
   end
 
   def my_friends
@@ -22,11 +23,16 @@ class UsersController < ApplicationController
               format.js { render partial: 'users/friend_result' }
           end
       end
-  else 
-      respond_to do |format|
-          flash.now[:alert] = "Please enter a user to search"
-          format.js { render partial: 'users/friend_result' }
-      end
+    else 
+        respond_to do |format|
+            flash.now[:alert] = "Please enter a user to search"
+            format.js { render partial: 'users/friend_result' }
+        end
+    end
   end
+
+  def show 
+    @user = User.find(params[:id])
+    @tracked_stocks = @user.stocks
   end
 end
